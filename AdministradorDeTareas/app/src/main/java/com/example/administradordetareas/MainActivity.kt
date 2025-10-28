970f0a0e5d7d3fef96cf1d0ddcd2e4bc397b6661
daaviddieeguez <daaviddieeguez@gmail.com>
2025-10-28 11:58:37 +0100

    fix: Cambio de texto a negrita

--- AdministradorDeTareas/app/src/main/java/com/example/administradordetareas/MainActivity.kt
+++ AdministradorDeTareas/app/src/main/java/com/example/administradordetareas/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,6 +50,7 @@ fun TareasCompletadas(tareas: String, enhorabuena: String, modifier: Modifier = 
     ) {
         Text(
             text = tareas,
+            fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
         )
         Text(
